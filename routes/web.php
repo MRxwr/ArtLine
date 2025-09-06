25a46528291498119f315bdcf90868e7593e84b0
MRxwr <nasserhatab@gmail.com>
2025-09-07 02:32:34 +0300

    Simplify routes for debugging 500 error
    - Removed controller imports temporarily
    - Added simple test route
    - Regenerated autoloader
    - Testing basic Laravel functionality

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\SuperDashboardController;
-use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test', function () {
+    return 'Laravel is working!';
 });
 
 // Admin Access Page
 Route::get('/admin/access', function () {
     return view('admin-access');
 })->name('admin.access');
-
-// Simple Admin Routes
-Route::get('/admin/superdashboard', [SuperDashboardController::class, 'index'])->name('admin.superdashboard');
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
