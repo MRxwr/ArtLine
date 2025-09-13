12846ff053e20f7876f32c6706cf379dc1313699
MRxwr <nasserhatab@gmail.com>
2025-09-13 23:56:00 +0300

    save

--- admin/includes/translate.php
+++ admin/includes/translate.php
@@ -16,22 +16,27 @@ $SettingsServiceCharge = $settings[0]["serviceCharge"];
 $googleCode = urldecode($settings[0]["google"]);
 $pixilCode = urldecode($settings[0]["pixil"]);
 
-$storeDetails = selectDBNew("stores",[$_GET["storeCode"]],"`id` = ?","");
-$headerButton = $storeDetails[0]["headerButton"];
-$websiteColor = $storeDetails[0]["websiteColor"];
-$settingsEmail = $storeDetails[0]["email"];
-$settingsPhone = $storeDetails[0]["phone"];
-$settingsTitle = $storeDetails[0]["title"];
-$settingsImage = $storeDetails[0]["bgImage"];
-$settingslogo = $storeDetails[0]["logo"];
-$showLogo = $storeDetails[0]["showLogo"];
-$settingsShippingMethod = $storeDetails[0]["shippingMethod"];
-$defaultCountry = $storeDetails[0]["country"];
-$settingsLang = (isset($storeDetails[0]["language"]) && $storeDetails[0]["language"] == "0") ? "ENG" : "AR";
-$productView = $storeDetails[0]["productView"];
-$showCategoryTitle = $storeDetails[0]["showCategoryTitle"];
-$categoryView = $storeDetails[0]["categoryView"];
-$theme = $storeDetails[0]["theme"];
+if( $storeDetails = selectDBNew("stores",[$_GET["storeCode"]],"`storeCode` = ?","") ){
+	$storeID = $storeDetails[0]["id"];
+	$headerButton = $storeDetails[0]["headerButton"];
+	$websiteColor = $storeDetails[0]["websiteColor"];
+	$settingsEmail = $storeDetails[0]["email"];
+	$settingsPhone = $storeDetails[0]["phone"];
+	$settingsTitle = $storeDetails[0]["title"];
+	$settingsImage = $storeDetails[0]["bgImage"];
+	$settingslogo = $storeDetails[0]["logo"];
+	$showLogo = $storeDetails[0]["showLogo"];
+	$settingsShippingMethod = $storeDetails[0]["shippingMethod"];
+	$defaultCountry = $storeDetails[0]["country"];
+	$settingsLang = (isset($storeDetails[0]["language"]) && $storeDetails[0]["language"] == "0") ? "ENG" : "AR";
+	$productView = $storeDetails[0]["productView"];
+	$showCategoryTitle = $storeDetails[0]["showCategoryTitle"];
+	$categoryView = $storeDetails[0]["categoryView"];
+	$theme = $storeDetails[0]["theme"];
+}else{
+	header ("LOCATION: default.php");die();
+}
+
 
 if ( isset($_GET["lang"]) ){
 	$arrayLangs = ["ENG","AR"];
