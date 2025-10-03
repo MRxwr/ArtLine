f177ef822be52ccddc816aed4cccf06826103c3e
MRxwr <nasserhatab@gmail.com>
2025-10-03 21:18:10 +0300

    Update logout.php

--- admin/logout.php
+++ admin/logout.php
@@ -1,4 +1,7 @@
 <?php
+include 'includes/config.php';
+include 'includes/functions.php';
+include 'includes/translate.php';
 setcookie($cookieSession."A", "", time() - (86400*30 ), "/");
 session_start ();
 if ( session_destroy() )
