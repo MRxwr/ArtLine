aa8777d111971bd5607c0046fa3aebeccb9ed7fc
MRxwr <nasserhatab@gmail.com>
2025-10-16 02:46:00 +0300

    save

---
+++ views/bladeBusy.php
@@ -0,0 +1,11 @@
+<div class="sec-pad">
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-6">
+            <?php 
+            echo direction("Our servers are having so much traffic at the moment, please try again later.", "خوادمنا تعاني من الكثير من الحركة في الوقت الحالي، يرجى المحاولة مرة أخرى لاحقًا.");
+            ?>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ views/bladeMaintenance.php
@@ -0,0 +1,11 @@
+<div class="sec-pad">
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-6">
+            <?php 
+            echo direction("We are doing some maintenance to our systems, we will be back soon.", "نقوم ببعض الصيانة على أنظمتنا، سنعود قريبًا.");
+            ?>
+            </div>
+        </div>
+    </div>
+</div>
