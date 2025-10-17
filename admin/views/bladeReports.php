aa9b052db3823a6b220cc413f917a4281d9cb25e
MRxwr <nasserhatab@gmail.com>
2025-10-18 02:41:54 +0300

    Update bladeReports.php

--- admin/views/bladeReports.php
+++ admin/views/bladeReports.php
@@ -64,7 +64,7 @@ input {
 			<label class="control-label mb-10"><?php echo direction("Stores","المتاجر") ?></label>
 			<?php
 				if( $stores = selectDB("stores", "`status` = '0' AND `hidden` = '0'") ){
-					echo "<select name='storeId' class='form-control' required style=''>";
+					echo "<select name='storeId' class='form-control' style=''>";
 					echo "<option value='' disabled selected>".direction("Select Store","حدد المتجر")."</option>";
 					for( $i = 0; $i < sizeof($stores); $i++ ){
 						echo "<option value='{$stores[$i]["id"]}' >{$stores[$i]["title"]}</option>";
