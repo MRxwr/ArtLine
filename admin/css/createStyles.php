0b3071f65ad0b2dddbc7969580244bb07c2673f8
MRxwr <nasserhatab@gmail.com>
2025-10-17 03:53:16 +0300

    Update createStyles.php

--- admin/css/createStyles.php
+++ admin/css/createStyles.php
@@ -21,4 +21,37 @@ input, textarea, select{
     border-radius: 10px !important;
     font-weight: 700;
 }
+.wrapper.theme-6-active .navbar.navbar-inverse.navbar-fixed-top {
+    background: #a39f8f;
+    border-bottom: 1px solid #ae9956;
+    box-shadow: 0px 0px 10px 3px;
+}
+.wrapper.theme-6-active .navbar.navbar-inverse.navbar-fixed-top .mobile-only-brand {
+    background: #a39f8f;
+}
+.wrapper.theme-6-active .fixed-sidebar-left .side-nav {
+    background: #a39f8fab;
+}
+.wrapper.theme-1-active .fixed-sidebar-left .side-nav > li > a, .wrapper.theme-3-active .fixed-sidebar-left .side-nav > li > a, .wrapper.theme-6-active .fixed-sidebar-left .side-nav > li > a {
+    color: #4e4933;
+}
+a.toggle-left-nav-btn i, a#toggle_mobile_nav i, a#toggle_mobile_search i {
+    font-size: 21px;
+    vertical-align: middle;
+    color: #4e4933;
+}
+.navbar.navbar-inverse.navbar-fixed-top .nav.top-nav > li > a i.top-nav-icon {
+    font-size: 24px;
+    vertical-align: middle;
+    color: #4e4933;
+}
+.wrapper.theme-6-active .navbar.navbar-inverse.navbar-fixed-top .nav-header .logo-wrap .brand-text {
+    color: #4e4933;
+}
+.navbar.navbar-inverse.navbar-fixed-top .nav-header .logo-wrap .brand-img {
+    margin-right: 10px;
+    position: relative;
+    top: 4px;
+    border-radius: 100px;
+}
 </style>
